7208ff3801d9a0a42e82ecbd03765493d3bd7dc6
gunnermadsen <gunner.madsen@outlook.com>
2020-08-11 20:39:51 -0700

    Initial Commit

---
+++ Compass/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Compass
+//
+//  Created by gunner madsen on 8/11/20.
+//  Copyright © 2020 Gunner Madsen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CompassTests/CompassTests.swift
@@ -0,0 +1,34 @@
+//
+//  CompassTests.swift
+//  CompassTests
+//
+//  Created by gunner madsen on 8/11/20.
+//  Copyright © 2020 Gunner Madsen. All rights reserved.
+//
+
+import XCTest
+@testable import Compass
+
+class CompassTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
